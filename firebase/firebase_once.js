fe36ecd8b57ecce278dc482d0a76a9703c95e19b
Austin Eldridge <AustinEldridge@Eaton.com>
2015-04-21 11:52:20 -0400

    Add shallow query support to Firebase.once().
    Also added some level of status notification for when a request is happening by flashing this.status to blue.

--- firebase/firebase_once.js
+++ firebase/firebase_once.js
@@ -1,5 +1,8 @@
 module.exports = function(RED) {
     'use strict';
+    var https = require("follow-redirects").https;
+    var urllib = require("url");
+
 
     function FirebaseOnce(n) {
         RED.nodes.createNode(this,n);
@@ -7,8 +10,10 @@ module.exports = function(RED) {
         this.config = RED.nodes.getNode(n.firebaseconfig);
         this.childpath = n.childpath;
         this.eventType = n.eventType;
+
         this.activeRequests = [];
         this.ready = false;
+        this.authData = null;
 
         // Check credentials
         if (!this.config) {
@@ -22,7 +27,8 @@ module.exports = function(RED) {
           "child_added": true,
           "child_changed": true,
           "chiled_removed": true,
-          "child_moved": true
+          "child_moved": true,
+          "shallow_query": true
         }
 
         this.onFBData = function(snapshot, prevChildName) {
@@ -44,12 +50,14 @@ module.exports = function(RED) {
             if(prevChildName)
               msg.previousChildName = prevChildName;
 
-            this.send(msg)
+            this.send(msg);
+            this.setStatus();
         }.bind(this);
 
         this.onFBError = function(error){
           this.error(error, {})
-          //TODO: Set status?
+          this.status({fill:"red",shape:"ring",text:error || "error"});
+          setTimeout(this.setStatus, 5000)  //Reset back to the Firebase status after 5 seconds
         }.bind(this);
 
         this.registerListeners = function(msg){
@@ -81,11 +89,18 @@ module.exports = function(RED) {
           msg.childpath = childpath || "/";
           this.activeRequests.push(msg)
 
-          if(msg.childpath){
-            this.config.fbConnection.fbRef.child(msg.childpath).once(eventType, this.onFBData, this.onFBError, this);
-          }else{
-            this.config.fbConnection.fbRef.once(eventType, this.onFBData, this.onFBError, this);
+          if(msg.eventType == "shallow_query"){
+            this.shallowQuery(msg)
+          } else {
+            this.status({fill:"blue",shape:"dot",text:"requesting from firebase..."});
+            if(msg.childpath){
+              this.config.fbConnection.fbRef.child(msg.childpath).once(eventType, this.onFBData, this.onFBError, this);
+            }else{
+              this.config.fbConnection.fbRef.once(eventType, this.onFBData, this.onFBError, this);
+            }
           }
+
+
         }.bind(this);
 
         this.destroyListeners = function(reason){
@@ -113,6 +128,79 @@ module.exports = function(RED) {
           }
         }.bind(this);
 
+        this.shallowQuery = function(msg){
+          this.status({fill:"blue",shape:"dot",text:"shallow_query requesting..."});
+
+          // make sure the path starts with '/'
+          var childpath = (msg.childpath.indexOf("/") == 0) ? msg.childpath : "/" + msg.childpath;
+          //make sure the path does not end with '/', (unless that is the string in its entirety)
+          childpath = (childpath.length != 1 && childpath.substr(-1) == "/") ? childpath.slice(0,-1) : childpath
+
+          msg.href = this.config.firebaseurl + childpath
+          var url =  msg.href + ".json?shallow=true"
+
+          if(this.authData && this.authData.token)
+            url += "&auth=" + this.authData.token
+
+          var opts = urllib.parse(url);
+          opts.method = "GET";;
+          //opts.headers = {};
+
+          var req = https.request(opts,function(res) {
+              res.setEncoding('utf8');
+              msg.statusCode = res.statusCode;
+              //msg.headers = res.headers;
+              msg.payload = "";
+              //msg.url = url;   // revert when warning above finally removed
+              res.on('data',function(chunk) {
+                  msg.payload += chunk;
+              }.bind(this));
+
+              res.on('end',function() {
+                  try { msg.payload = JSON.parse(msg.payload); }
+                  catch(e) { this.warn("JSON parse error"); }
+
+                  if(msg.statusCode != 200){
+                    this.error(msg.payload.error || "firebase shallow_query error", msg)
+                    this.status({fill:"red",shape:"ring",text:msg.payload.error || "error"});
+                    setTimeout(this.setStatus, 5000)  //Reset back to the Firebase status after 5 seconds
+                  } else {
+                    this.send(msg);
+                    this.setStatus();
+                  }
+              }.bind(this));
+          }.bind(this));
+
+          req.on('error',function(err) {
+              msg.statusCode = err.code;
+              this.error(err.toString() + " : " + url, msg);
+              this.status({fill:"red",shape:"ring",text:err.code});
+              setTimeout(this.setStatus, 5000)  //Reset back to the Firebase status after 5 seconds
+          }.bind(this));
+
+          req.end();
+        }.bind(this);
+
+        this.setStatus = function(){
+          //set = state (depending on the deployment strategy, for newly deployed nodes, some of the events may not be refired...)
+          switch(this.config.fbConnection.lastEvent) {
+            case "connecting":
+            case "connected":
+            case "disconnected":
+            case "authorized":
+            case "unauthorized":
+            case "error":
+            case "closed":
+              this["fb" + this.config.fbConnection.lastEvent.capitalize()](this.config.fbConnection.lastEventData)  //Javascript is really friendly about sending arguments to functions...
+              break;
+            // case "undefined":
+            // case "null":
+            //   break;  //Config node not yet setup
+            default:
+              this.error("Bad lastEvent Data from Config Node - " + this.config.fbConnection.lastEvent)
+          }
+
+        }.bind(this)
 
         //this.config.fbConnection EventEmitter Handlers
         this.fbConnecting = function(){  //This isn't being called because its emitted too early...
@@ -137,6 +225,7 @@ module.exports = function(RED) {
           // this.log("authorized: " + JSON.stringify(authData))
           this.status({fill:"green", shape:"dot", text:"ready"})
           this.ready = true;
+          this.authData = authData
         }.bind(this)
 
         this.fbUnauthorized = function(){
@@ -144,6 +233,7 @@ module.exports = function(RED) {
           this.status({fill:"red", shape:"dot", text:"unauthorized"})
           this.ready = false;
           this.destroyListeners();
+          this.authData = null;
         }.bind(this)
 
         this.fbError = function(error){
@@ -169,23 +259,7 @@ module.exports = function(RED) {
         this.config.fbConnection.on("error", this.fbError)
         this.config.fbConnection.on("closed", this.fbClosed)
 
-        //set initial state (depending on the deployment strategy, for newly deployed nodes, some of the events may not be refired...)
-        switch(this.config.fbConnection.lastEvent) {
-          case "connecting":
-          case "connected":
-          case "disconnected":
-          case "authorized":
-          case "unauthorized":
-          case "error":
-          case "closed":
-            this["fb" + this.config.fbConnection.lastEvent.capitalize()](this.config.fbConnection.lastEventData)  //Javascript is really friendly about sending arguments to functions...
-            break;
-          // case "undefined":
-          // case "null":
-          //   break;  //Config node not yet setup
-          default:
-            this.error("Bad lastEvent Data from Config Node - " + this.config.fbConnection.lastEvent)
-        }
+        this.setStatus()
 
         this.on('input', function(msg) {
             if(this.ready){
